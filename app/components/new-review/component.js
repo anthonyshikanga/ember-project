83d6f23131c546c29aea19366e7109047994c129
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-23 02:44:06 +0300

    modified new-review component

--- app/components/new-review/component.js
+++ app/components/new-review/component.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewReview: false,
+  actions: {
+    cancelReviewForm(){
+      this.set('addNewReview',false)
+    },
+    reviewFormShow() {
+      this.set('addNewReview', true);
+    },
+    saveReview() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        movie: this.get('movie'),
+        revieweDate: new Date(),
+      };
+      this.set('author',null);
+this.set('content',null);
+      this.set('addNewReview', false);
+      this.sendAction('saveReview', params);
+    }
+  }
 });
