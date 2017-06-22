89d8ced6a091ec95d194e74a21af8358ee926838
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-22 13:21:26 +0300

    added atrributes to  model files

--- app/models/movie.js
+++ app/models/movie.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  director:DS.attr(),
+  name:DS.attr(),
+  description:DS.attr(),
+  actor:DS.attr(),
+  year:DS.attr(),
+  rating:DS.attr(),
+  image:DS.attr(),
+  stream:DS.attr(),
+  reviews: DS.hasMany('review', { async: true }),
 
 });
