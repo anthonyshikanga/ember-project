8eb0351d6fdab4348c4392199b34240d4b5d8503
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-23 02:59:01 +0300

    Added format-reviews helper

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      movies: this.store.findAll('movie'),
+      reviews: this.store.findAll('review')
+    });
+  },
+  actions: {
+    saveMovie3(params) {
+      var newMovie = this.store.createRecord('movie', params);
+      newMovie.save();
+      this.transitionTo('index');
+    },
+  }
 });
