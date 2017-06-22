0585e7c2a968a733fedff9c76c36e20a40f5f7aa
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-23 02:30:08 +0300

    Added update-movie component

--- app/components/update-movie/component.js
+++ app/components/update-movie/component.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateMovieForm: false,
+  actions: {
+    cancelMovieForm(){
+      this.set('updateMovieForm',false);
+    },
+    updateMovieForm() {
+      this.set('updateMovieForm', true);
+    },
+    update(movie) {
+      var params = {
+        name: this.get('name'),
+        year: this.get('year'),
+        description: this.get('description'),
+        director: this.get('director'),
+        rating: this.get('rating'),
+        image: this.get('image'),
+        stream: this.get('stream'),
+        actor: this.get('actor'),
+
+      };
+      this.set('updateMovieForm', false);
+      this.sendAction('update', movie, params);
+    }
+}
 });
