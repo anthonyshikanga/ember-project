26331be356523ebeb235211eaf6d6ad02a9b6f8b
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-22 17:00:29 +0300

    Modified new-movie js

--- app/components/new-movie/component.js
+++ app/components/new-movie/component.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewMovie: false,
+  actions: {
+    cancelMovieForm(){
+      this.set('addNewMovie',false);
+    },
+    movieFormShow() {
+      this.set('addNewMovie', true);
+    },
+
+    saveMovie1() {
+      var params = {
+        name: this.get('name'),
+        year: this.get('year'),
+        description: this.get('description'),
+        director: this.get('director'),
+        rating: this.get('rating'),
+        image: this.get('image'),
+        stream: this.get('stream'),
+
+
+      };
+      this.set('name',null);
+      this.set('year',null);
+      this.set('description',null);
+      this.set('director',null);
+      this.set('image',null);
+      this.set('stream',null);
+      this.set('rating',null);
+      this.set('addNewMovie', false);
+      this.sendAction('saveMovie2', params);
+    }
+  }
 });
